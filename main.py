f90cfb04d4168e4753e6fe3a6c2624c378a55c3a
Lada420 <repero11@hotmail.com>
2020-11-26 09:05:12 +0300

    Fix for windows daemon compatibility

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ if __name__ == "__main__":
     args = get_args()
 
     if args.d:
-        with daemon.DaemonContext():
-            file_log_info("Start as a daemon")
-            presence_marker(sys.argv[1], sys.argv[2])
-            file_log_info("Goodbye from daemon")
-            exit(SUCCESS)
+        if "Linux" in platform.system():
+            import daemon
+            with daemon.DaemonContext():
+                file_log_info("Start as a daemon")
+                presence_marker(args.login, args.password)
+                file_log_info("Goodbye from daemon")
+                exit(SUCCESS)
+        elif "Windows" in platform.system():
+            con_log_warn("Feature not implemented. Running as app")
 
     con_log_info("Start as an application")
-    presence_marker(sys.argv[1], sys.argv[2])
+    presence_marker(args.login, args.password)
     con_log_info("Goodbye from application")
     exit(SUCCESS)
